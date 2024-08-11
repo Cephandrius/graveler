556bde1cfc8080ad58ccfaf50eed07865f78c14e
Paul Christopher Schuck Jr <paulchristopherschuckjr@gmail.com>
2024-08-11 11:41:06 -0600

    The code that I think will the run the fastest on python without multi processing

--- src/python/simulate.py
+++ src/python/simulate.py
@@ -1,26 +1,25 @@
 import random
 import math
-from itertools import repeat
 from line_profiler import profile
 
 @profile
 def main():
-	items = [1,2,3,4]
-	numbers = [0,0,0,0]
-	rolls = 0
-	maxOnes = 0
-	
-	while numbers[0] < 177 and rolls < 10000:
-	    numbers = [0,0,0,0]
-	    for i in repeat(None, 231):
-	        roll = random.choice(items)
-	        numbers[roll-1] = numbers[roll-1] + 1
-	    rolls = rolls + 1
-	    if numbers[0] > maxOnes:
-	        maxOnes = numbers[0]
-	
-	print("Highest Ones Roll:",maxOnes)
-	print("Number of Roll Sessions: ",rolls)
+    rolls = 0
+    maxOnes = 0
+    ones = 0
+    
+    while ones < 177 and rolls < 10000:
+        ones = 0
+        for i in range(231):
+            roll = random.randint(0,3)
+            if roll == 0:
+                  ones += 1
+        rolls = rolls + 1
+        if ones > maxOnes:
+            maxOnes = ones
+    
+    print("Highest Ones Roll:",maxOnes)
+    print("Number of Roll Sessions: ",rolls)
 
 if __name__ == "__main__":
-	main()
+    main()
